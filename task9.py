be77e0d17d7912d670a14c1849b7494cf9b91545
DimaKravec <dimakravec228@gmail.com>
2023-01-19 15:46:22 +0200

    task1

--- task9.py
+++ task9.py
@@ -8,43 +8,61 @@ while user == 'R' or 'S' or 'P':
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'S' and random_RSP == 'P':
         print(f"User - {user}, Bot - {random_RSP}, USER WINNER")
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'P' and random_RSP == 'P':
         print(f"User - {user}, Bot - {random_RSP}, USER WINNER")
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'R' and random_RSP == 'R':
         print(f'User - {user}, Bot - {random_RSP}, DRAW!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'S' and random_RSP == 'S':
         print(f'User - {user}, Bot - {random_RSP}, DRAW!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'P' and random_RSP == 'P':
         print(f'User - {user}, Bot - {random_RSP}, DRAW!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'R' and not random_RSP == 'S':
         print(f'User - {user}, Bot - {random_RSP}, LOSE!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'S' and not random_RSP == 'P':
         print(f'User - {user}, Bot - {random_RSP}, LOSE!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
     elif user == 'P' and not random_RSP == 'R':
         print(f'User - {user}, Bot - {random_RSP}, LOSE!')
         send = input("Maybe again? Yes/No ")
         if send == 'No':
             break
+        elif send == 'Yes':
+            user = input("Enter symbols 'R' - Rock; 'S' - scissors; 'P' - paper: ")
